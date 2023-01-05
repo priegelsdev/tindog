b8c5393ee54b917a99eff0fa832cfd3a3b53560a
priegelsdev <priegelsdev@gmail.com>
2023-01-05 15:47:22 +0100

    disable buttons when no dog left in array

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import Dog from '/Dog.js';
 import dogs from '/data.js';
 
 let dog = getNewDog();
+let dogImg = document.getElementById('dog').getElementsByTagName('img')
+
 
 // event listener
 
@@ -14,23 +16,36 @@ document.addEventListener('click', function(e) {
 // function to handle swipe and set boolean in data
 
 function handleSwipe(e) {
-  if (e.target.id === 'nope-btn') {
-    dog.hasBeenSwiped = true;    
-  } else if (e.target.id === 'like-btn') {
+  if (e.target.id === 'nope-btn' && dogImg.length > 0) {
+    dog.hasBeenSwiped = true; 
+    handleNope();   
+  } else if (e.target.id === 'like-btn' && dogImg.length > 0) {
     dog.hasBeenSwiped = true;
     dog.hasBeenLiked = true;
+    handleLike();
   }
 
   if (dogs.length > 0) {
     dog = getNewDog();
-    render();
+      setTimeout(() => render(), 1500);
   } else {
-    document.getElementById('dog').innerHTML = `
-      <div style="height: 79vh; display: flex; align-items: center; justify-content: center">
-      <p class="no-dogs">No more doggies in your area.</p>
-    `
+      setTimeout(() => document.getElementById('dog').innerHTML = `
+        <div style="height: 79vh; display: flex; align-items: center; justify-content: center">
+        <p class="no-dogs">No more doggies in your area.</p>
+      `, 1500);
   }
+}
 
+// function to handle like 
+
+function handleLike() {
+  document.getElementById('dog').innerHTML += `<img class='badge' src='/img/badge-like.png'>`
+}
+
+// function to handle nope
+
+function handleNope() {
+  document.getElementById('dog').innerHTML += `<img class='badge' src='/img/badge-nope.png'>`
 
 }
 
